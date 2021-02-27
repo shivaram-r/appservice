21d4c45805e47f7892315fc7a8b7c2ba433d70ae
Shiva <71781502+shivaram-r@users.noreply.github.com>
2021-02-27 23:12:02 +0530

    null check no no

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace appservice.Controllers
 
         public IActionResult Privacy()
         {
-            List<string> lString;
+            List<string> lString = null;
             lString.Add("t");
             return View();
         }
